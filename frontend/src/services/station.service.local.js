d8c4bc44631f05db924586ba56825600a4641fb2
Shachak Armon <shachak.armon@gmail.com>
2023-01-18 13:50:02 +0200

    changed the services to "station"

--- frontend/src/services/station.service.local.js
+++ frontend/src/services/station.service.local.js
@@ -3,69 +3,69 @@ import { storageService } from './async-storage.service.js'
 import { utilService } from './util.service.js'
 import { userService } from './user.service.js'
 
-const STORAGE_KEY = 'car'
+const STORAGE_KEY = 'station'
 
-export const carService = {
+export const stationService = {
     query,
     getById,
     save,
     remove,
-    getEmptyCar,
-    addCarMsg
+    getEmptyStation,
+    addStationMsg
 }
-window.cs = carService
+window.cs = stationService
 
 
 async function query(filterBy = { txt: '', price: 0 }) {
-    var cars = await storageService.query(STORAGE_KEY)
+    var stations = await storageService.query(STORAGE_KEY)
     if (filterBy.txt) {
         const regex = new RegExp(filterBy.txt, 'i')
-        cars = cars.filter(car => regex.test(car.vendor) || regex.test(car.description))
+        stations = stations.filter(station => regex.test(station.vendor) || regex.test(station.description))
     }
     if (filterBy.price) {
-        cars = cars.filter(car => car.price <= filterBy.price)
+        stations = stations.filter(station => station.price <= filterBy.price)
     }
-    return cars
+    return stations
 }
 
-function getById(carId) {
-    return storageService.get(STORAGE_KEY, carId)
+function getById(stationId) {
+    return storageService.get(STORAGE_KEY, stationId)
 }
 
-async function remove(carId) {
+async function remove(stationId) {
     // throw new Error('Nope')
-    await storageService.remove(STORAGE_KEY, carId)
+    await storageService.remove(STORAGE_KEY, stationId)
 }
 
-async function save(car) {
-    var savedCar
-    if (car._id) {
-        savedCar = await storageService.put(STORAGE_KEY, car)
+async function save(station) {
+    var savedStation
+    if (station._id) {
+        savedStation = await storageService.put(STORAGE_KEY, station)
     } else {
         // Later, owner is set by the backend
-        car.owner = userService.getLoggedinUser()
-        savedCar = await storageService.post(STORAGE_KEY, car)
+        station.owner = userService.getLoggedinUser()
+        savedStation = await storageService.post(STORAGE_KEY, station)
     }
-    return savedCar
+    return savedStation
 }
 
-async function addCarMsg(carId, txt) {
+async function addStationMsg(stationId, txt) {
     // Later, this is all done by the backend
-    const car = await getById(carId)
-    if (!car.msgs) car.msgs = []
+    const station = await getById(stationId)
+    if (!station.msgs) station.msgs = []
 
     const msg = {
         id: utilService.makeId(),
         by: userService.getLoggedinUser(),
-        txt
+        txt: txt,
     }
-    car.msgs.push(msg)
-    await storageService.put(STORAGE_KEY, car)
+    station.msgs.push(msg)
+    await storageService.put(STORAGE_KEY, station)
 
     return msg
 }
 
-function getEmptyCar() {
+function getEmptyStation() {
     return {
         vendor: 'Susita-' + (Date.now() % 1000),
         price: utilService.getRandomIntInclusive(1000, 9000),
