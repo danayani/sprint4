cb037dbf8a34f1ee528d47a5fd02e4e6bc73e7e5
Dana Yaniv <danayani@gmail.com>
2023-01-18 09:12:05 +0200

    organization page

--- frontend/src/root-cmp.jsx
+++ frontend/src/root-cmp.jsx
@@ -4,12 +4,10 @@ import { Routes, Route } from 'react-router'
 import routes from './routes'
 
 import { AppHeader } from './cmps/app-header'
-import { AppFooter } from './cmps/app-footer'
 import { UserDetails } from './pages/user-details'
 import { HomePage } from './pages/home-page'
 
 export function RootCmp() {
-
     return (
         <div>
             <AppHeader />
@@ -19,12 +17,11 @@ export function RootCmp() {
                     <Route path="user/:id" element={<UserDetails />} /> */}
 
                     <Route path="/" element={<HomePage />} />
-                    <Route path='/library' element={<Library />} />
-                    <Route path="/search" element={<SearchPage />} />
+                    {/* <Route path='/library' element={<Library />} />
+                    <Route path="/search" element={<SearchPage />} /> */}
 
                 </Routes>
             </main>
-            <AppFooter />
         </div>
     )
 }
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,7 +2,7 @@ import { HomePage } from './pages/home-page.jsx'
 import { AboutUs } from './pages/about-us.jsx'
 import { CarIndex } from './pages/library.jsx'
 import { ReviewIndex } from './pages/review-index.jsx'
-import { AdminApp } from './pages/admin-app.jsx'
+// import { AdminApp } from './pages/admin-app.jsx'
 
 // Routes accesible from the main navigation (in AppHeader)
 const routes = [
@@ -25,12 +25,12 @@ const routes = [
         path: 'about',
         component: <AboutUs />,
         label: 'About us'
-    },
-    {
-        path: 'admin',
-        component: <AdminApp />,
-        label: 'Admin Only'
     }
+    // {
+    //     path: 'admin',
+    //     component: <AdminApp />,
+    //     label: 'Admin Only'
+    // }
 ]
 
 export default routes
