c0f7ab9b9fc322875c169000cd6c3ad068c4c384
Shelly Paxton Eliyahu <shellypax@gmail.com>
2023-01-19 20:57:46 +0200

    app header

--- frontend/src/cmps/app-header.jsx
+++ frontend/src/cmps/app-header.jsx
@@ -1,15 +1,24 @@
 // navigate arrows + user options
-
 //TODO : add link to about page
-//TODO : navigate arrows
 //acording to the url (page) the headder change
-
-
+import left from '../assets/icons/left.png';
+import right from '../assets/icons/right.png';
+import {useNavigate} from "react-router-dom";
 export function AppHeader() {
+    const navigate = useNavigate()
+
+    function onGo(diff) {
+        navigate(diff)
+    }
     return (
         <div className="top-bar-container">
             <header className="app-header">
-                <h2>header cmp</h2>
+                <button className="go-btn" onClick={()=>onGo(-1)}>
+                    <img className='btn-icon' src={left} />
+                </button>
+                <button className="go-btn" onClick={()=>onGo(1)}>
+                    <img className='btn-icon' src={right} />
+                </button>
             </header>
 
         </div>
