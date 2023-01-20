ea02dc286b686fc6a1a4525c9550bb34323be96f
Dana Yaniv <danayani@gmail.com>
2023-01-20 08:41:11 +0200

    video

--- frontend/src/pages/library.jsx
+++ frontend/src/pages/library.jsx
@@ -111,8 +111,9 @@ export function Library() {
             <h3>Music </h3>
 
             {itemList2.map((item) => {
+                console.log('item video', item.)
                 return <iframe key={item.id} id="player" type="text/html" width="640" height="390"
-                    src={`http://www.youtube.com/embed/${item.id}`}
+                    src={`http://www.youtube.com/embed/YykjpeuMNEk`}
                     frameBorder="0"></iframe>
             })}
         </div>
