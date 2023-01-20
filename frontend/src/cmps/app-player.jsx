cdcc63d7a8cd83d89f49c995a8b64a6b7985ff07
Dana Yaniv <danayani@gmail.com>
2023-01-20 17:06:19 +0200

    player standing

--- frontend/src/cmps/app-player.jsx
+++ frontend/src/cmps/app-player.jsx
@@ -11,16 +11,25 @@ import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
 
 export function AppPlayer() {
     return (
-        <div className="app-playerS grid">
-            <div className="song-details">
-                <img className="song-img" src='../assets/img/rh.jpg'/>
-                <p className="song-name">song name</p>
-                <p className="song-artist">song artist</p>
-            </div>
-            
+        <div className="app-playerS flex">
             <iframe className='video-player' id="player" type="text/html" width="100" height="70"
                 src={`http://www.youtube.com/embed/QtXby3twMmI`}
                 frameBorder="0"></iframe>
+
+            <div className="song-details flex">
+                <img className="song-img" src='../assets/img/rh.jpg' />
+                <p className="song-name">song name</p>
+                <p className="song-artist">song artist</p>
+            </div>
+
+            <div className="actions flex justify-center">
+                on/off
+            </div>
+
+            <div className="volume-controller flex">
+                volume
+            </div>
+
 
         </div>
     )
