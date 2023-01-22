b99385e70b5806f7012c323a9f2e489ff777a148
Dana Yaniv <danayani@gmail.com>
2023-01-22 22:40:47 +0200

    song list

--- frontend/src/cmps/song-list.jsx
+++ frontend/src/cmps/song-list.jsx
@@ -13,57 +13,37 @@ export function SongList() {
         stationService.getById(stationId).then(res => {
             const songsList = res.songs
             setStationSongs(songsList)
-
         })
     }, [])
     console.log('station ', stationId, ' : ', stationSongs)
 
-
-
-
-    return (
-
+    if (!stationSongs) return (<h1> loading...</h1>)
+    else return (
+        <h1>hi</h1>
         // <h2>song list for station : {stationId} </h2>
-        <tbody className="song-list-tbody">
-            <tr>
-                <th className="song-list-tbodyHashtag" >#</th>
-                <th className="song-list-title">TITLE</th>
-                <th>DATE ADDED</th>
-                <th><i className="song-list-tbodyTime fa-regular fa-clock"></i></th>
-            </tr>
-            {/* {mails.map(mail => <li className="mail-item" key={mail.id}>
-                <MailPreview onRemoveMail={onRemoveMail} mail={mail} />
-            </li>)} */}
+        // <tbody className="song-list-tbody">
+        //     <tr>
+        //         <th className="song-list-tbodyHashtag" >#</th>
+        //         <th className="song-list-title">TITLE</th>
+        //         <th>DATE ADDED</th>
+        //         <th><i className="song-list-tbodyTime fa-regular fa-clock"></i></th>
+        //     </tr>
+        //     {/* {mails.map(mail => <li className="mail-item" key={mail.id}>
+        //         <MailPreview onRemoveMail={onRemoveMail} mail={mail} />
+        //     </li>)} */}
 
-            {console.log('station', stationSongs)}
-            {stationSongs.map(station => { 
-                return<tr key={station.id}>
-                    <td></td>
-                    <td><button className="btn-song-title-play">{station.title}</button></td>
-                    <td>{station.addedAt}</td>
-                    <td>03:15</td>
-                </tr>
-            })
-            }
-            {/* <tr>
-                <td>1</td>
-                <td>coldplay - bla bla bla</td>
-                <td> 14.3.1997</td>
-                <td> 03:15</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>coldplay - bla bla bla</td>
-                <td> 14.3.1997</td>
-                <td> 03:15</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>coldplay - bla bla bla</td>
-                <td> 14.3.1997</td>
-                <td> 03:15</td>
-            </tr> */}
-        </tbody>
+        //     {console.log('station', stationSongs)}
+        //     {stationSongs.map(station => {
+        //         return <tr key={station.id}>
+        //             <td></td>
+        //             <td><button className="btn-song-title-play">{station.title}</button></td>
+        //             <td>{station.addedAt}</td>
+        //             <td>03:15</td>
+        //         </tr>
+        //     })
+        //     }
+
+        // </tbody>
 
 
     )
