76644fa1bb5a5ce8f45f82815de30369e1a2b47d
Dana Yaniv <danayani@gmail.com>
2023-01-22 14:11:14 +0200

    rander songs list

--- frontend/src/cmps/song-list.jsx
+++ frontend/src/cmps/song-list.jsx
@@ -22,35 +22,47 @@ export function SongList() {
 
 
     return (
-        
-            // <h2>song list for station : {stationId} </h2>
-            <tbody className="song-list">
-                <tr>
-                    <th>#</th>
-                    <th>TITLE</th>
-                    <th>DATE ADDED</th>
-                    <th><i className="fa-regular fa-clock"></i></th>
+
+        // <h2>song list for station : {stationId} </h2>
+        <tbody className="song-list">
+            <tr>
+                <th>#</th>
+                <th>TITLE</th>
+                <th>DATE ADDED</th>
+                <th><i className="fa-regular fa-clock"></i></th>
+            </tr>
+            {/* {mails.map(mail => <li className="mail-item" key={mail.id}>
+                <MailPreview onRemoveMail={onRemoveMail} mail={mail} />
+            </li>)} */}
+
+            {console.log('station', station)}
+            {station.map(station => { 
+                return<tr>
+                    <td></td>
+                    <td>{station.title}</td>
                 </tr>
-                <tr>
-                    <td>1</td>
-                    <td>coldplay - bla bla bla</td>
-                    <td> 14.3.1997</td>
-                    <td> 3M7S</td>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>coldplay - bla bla bla</td>
-                    <td> 14.3.1997</td>
-                    <td> 3M7S</td>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>coldplay - bla bla bla</td>
-                    <td> 14.3.1997</td>
-                    <td> 3M7S</td>
-                </tr>
-            </tbody>
-        
+            })
+            }
+            <tr>
+                <td>1</td>
+                <td>coldplay - bla bla bla</td>
+                <td> 14.3.1997</td>
+                <td> 3M7S</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>coldplay - bla bla bla</td>
+                <td> 14.3.1997</td>
+                <td> 3M7S</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>coldplay - bla bla bla</td>
+                <td> 14.3.1997</td>
+                <td> 3M7S</td>
+            </tr>
+        </tbody>
+
 
     )
 }
