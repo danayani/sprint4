26a9817c8073d89d0c9ba668d15bd7a50c092038
Dana Yaniv <danayani@gmail.com>
2023-01-22 12:29:55 +0200

    station

--- frontend/src/cmps/song-list.jsx
+++ frontend/src/cmps/song-list.jsx
@@ -1,5 +1,32 @@
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { stationService } from "../services/station.service"
+
+//TODO : service function 'getSongListByStationId'
+export function SongList() {
+
+    const { stationId } = useParams()
+    const [station, setStation] = useState([])
+
+    useEffect(() => {
+        stationService.getById(stationId).then(res => {
+            const songsList = res.songs
+            setStation(songsList)
+
+        })
+    }, [])
+    console.log('station ', stationId, ' : ', station)
 
 
-export function SongList () {
+    
 
+    return (
+        <div>
+            <h2>song list for station : {stationId} </h2>
+            <ul className="song-list">
+                {/* <p> {station} </p> */}
+                {console.log('station song', )}
+            </ul>
+        </div>
+    )
 }
