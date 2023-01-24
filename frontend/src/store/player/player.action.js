82f16e393af2d4ce4734813604c7ce0fdfd2cb1e
Dana Yaniv <danayani@gmail.com>
2023-01-25 00:27:16 +0200

    player store

--- frontend/src/store/player/player.action.js
+++ frontend/src/store/player/player.action.js
@@ -1,21 +1,34 @@
 import { playerService } from '../../services/player.service.js';
+import { stationService } from '../../services/station.service.js';
 import { store } from '../store.js'
-import { PLAY_PAUSE_PLAYER, NEXT_SONG, PREVIOUS_SONG, REMOVE_SONG_PLAYER } from './player.reducer'
+import {LOAD_STATION_FOR_PLAYER, PLAY_PAUSE_PLAYER, NEXT_SONG, PREVIOUS_SONG, REMOVE_SONG_PLAYER } from './player.reducer.js'
 
 export async function loadPlayer(player) {
     try {
         console.log('player from actions:', player);
         // store.dispatch({ type: SET_PLAYER, player })
     } catch (err) {
-        console.log('Cant load player', err)
+        console.error(err)
         throw err
     }
 }
 
+
+export async function loadcurrPlayingStation(stationId) {
+    try {
+        console.log('loadcurrPlayingStation:', stationId)
+        // store.dispatch({ type: LOAD_STATION_FOR_PLAYER, station : stationService.getById(stationId) })
+    } catch (err) {
+        console.error(err)
+        throw err
+    }
+}
+
+
 export function getActionPlayPausePlayer() {
-    console.log('action from cmp player')
     return store.dispatch({ type: PLAY_PAUSE_PLAYER })
 }
+
 //   export function getActionAddReview(review) {
 //     return { type: 'ADD_REVIEW', review }
 //   }
