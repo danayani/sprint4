7ca62c8d4febc76c9fccdd1743fba3ba7ebef622
Dana Yaniv <danayani@gmail.com>
2023-01-25 15:18:19 +0200

    loader style + player store functuon

--- frontend/src/store/player/player.reducer.js
+++ frontend/src/store/player/player.reducer.js
@@ -1,10 +1,13 @@
 import { playerService } from "../../services/player.service"
 
 export const PLAY_PAUSE_PLAYER = 'PLAY_PLAYER'
+export const LOAD_SONGS = 'LOAD_SONGS'
 export const MOVE_SONG = 'MOVE_SONG'
 export const LOAD_STATION_FOR_PLAYER = 'LOAD_STATION_FOR_PLAYER'
+export const VOLUME_CHANGE = 'VOLUME_CHANGE'
+export const SHUFFLE_SONGS = 'SHUFFLE_SONGS'
 
-const initialState = { 
+const initialState = {
     currPlayingStation: {},
     playerState: playerService.getDefaultState(),
     songs: [],
@@ -13,15 +16,22 @@ const initialState = {
 
 export function playerReducer(state = initialState, action) {
     let playerState
+    let songs
     switch (action.type) {
         case PLAY_PAUSE_PLAYER:
             playerState = { ...state.playerState, playing: !state.playerState.playing }
             return { ...state, playerState }
+        case LOAD_SONGS:
+            songs = currPlayingStation.songs
+            return { ...state, songs }
         case MOVE_SONG:
             console.log('NEXT_SONG')
             return
         case LOAD_STATION_FOR_PLAYER:
-            return { ...state, currPlayingStation : action.station }
+            return { ...state, currPlayingStation: action.station }
+        case VOLUME_CHANGE:
+            return
+
         default:
             return state
     }
