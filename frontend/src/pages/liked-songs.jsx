3d57c2f2fabd9f2931fd08a4301e95430b77289b
Shelly Paxton Eliyahu <shellypax@gmail.com>
2023-01-25 21:34:30 +0200

    liked page start

--- frontend/src/pages/liked-songs.jsx
+++ frontend/src/pages/liked-songs.jsx
@@ -1,11 +1,11 @@
-// import { Loader } from "../cmps/loader.jsx"
+import { Loader } from "../cmps/loader.jsx"
 
-// export function LikedSongs() {
-//     const
+export function LikedSongs() {
+    const [likedSongs,setLikedSongs] = useState([])
 
 
-//     if(!songs) <Loader/>
-//     return (
-//         <h1>hi from liked songs</h1>
-//     )
-// }
+    if(!songs) <Loader/>
+    return (
+        <h1>hi from liked songs</h1>
+    )
+}
