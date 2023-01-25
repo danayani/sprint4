1cf120a9f5baa90df15bbc7eb91de17c114a9c7f
Dana Yaniv <danayani@gmail.com>
2023-01-25 10:44:00 +0200

    improve

--- frontend/src/cmps/station-header.jsx
+++ frontend/src/cmps/station-header.jsx
@@ -21,7 +21,7 @@ export function StationHeader() {
     }, [stationId])
 
     function onPlayStation(){
-        loadCurrPlayingStation(stationId)
+        // loadCurrPlayingStation(stationId)
     }
 
 
