2ab7cf7f716a79a972d97a93a15eb0023036a41c
Shelly Paxton Eliyahu <shellypax@gmail.com>
2023-01-25 12:59:06 +0200

    pixel perfect

--- frontend/src/cmps/second-stations-list.jsx
+++ frontend/src/cmps/second-stations-list.jsx
@@ -9,14 +9,14 @@ export function SecondStationList({ stations }) {
                     <Link key={station._id} to={`/station/${station._id}`}>
                         <div className="second-station-card">
                             {/* <StationDetails /> */}
-                            <img className="song-img" src={station.songs[0].imgUrl} alt="song img" />
+                            <img className="song-card-img" src={station.songs[0].imgUrl} alt="song img" />
                             <h3>{station.name}</h3>
                             {/* <button className='play'>Play</button> */}
                         </div>
                     </Link>
                 )}
             </section>
-            <h2>Recommended songs</h2>
+            <h2 className="station-extra">Recommended songs</h2>
         </section>
     )
 
