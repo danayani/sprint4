c19e34503bf61b1dcf2d2c47e2b26607373a9be7
Shachak Armon <shachak.armon@gmail.com>
2023-01-29 16:11:50 +0200

    demo data and style

--- frontend/src/services/util.service.js
+++ frontend/src/services/util.service.js
@@ -17,7 +17,7 @@ export const utilService = {
 }
 
 function timeConverter(UNIX_timestamp){
-    var a = new Date(UNIX_timestamp * 1000);
+    var a = new Date(UNIX_timestamp);
     var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
     var year = a.getFullYear();
     var month = months[a.getMonth()];
