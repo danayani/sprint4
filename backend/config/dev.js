e79147b48edfc59229b48acf723867055e9e3095
Shelly Paxton Eliyahu <shellypax@gmail.com>
2023-01-29 22:21:08 +0200

    debugg

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,4 +1,5 @@
 module.exports = {
-  dbURL: 'mongodb://127.0.0.1:27017', 
+  // dbURL: 'mongodb://127.0.0.1:27017', 
+  dbURL: 'mongodb+srv://shelly:ySdcmcoEEjSpayge@cluster0.pfzyanl.mongodb.net/?retryWrites=true&w=majority',
   dbName : 'melody_db'
 }
