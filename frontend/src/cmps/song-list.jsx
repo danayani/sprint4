7b1d766ee5ea0a54edfded012301f8da5192d7b7
Dana Yaniv <danayani@gmail.com>
2023-01-30 12:08:45 +0200

    song list

--- frontend/src/cmps/song-list.jsx
+++ frontend/src/cmps/song-list.jsx
@@ -64,36 +64,37 @@ export function SongList({ station, playStation }) {
                     const classSvgLiked = (song.liked) ? 'song-liked-svg' : 'song-dis-liked-svg'
                     const titleSvgLiked = (!song.liked) ? 'add to Liked Songs' : 'remove from Liked Songs'
                     return <li key={song.id} className="song-list-li grid" onMouseOver={() => onMouseSongLine(1)} onMouseOut={() => onMouseSongLine(0)}>
-                            <div className="btn-song-list-play">
-                                {idx + 1}
+                        <div className="btn-song-list-play">
+                            {idx + 1}
+                        </div>
+                        <section role="button" title="Play" onClick={() => onPlaySong(idx)} className="song-details">
+                            <div >
+                                <img className="song-list-img" src={song.imgUrl} />
                             </div>
-                            <section role="button" title="Play" onClick={() => onPlaySong(idx)} className="song-details">
-                                <div >
-                                    <img className="song-list-img" src={song.imgUrl} />
+                            <div className="song-info">
+                                <div className="song-list-title">
+                                    <p>{song.title}</p>
                                 </div>
-                                <div className="song-info">
-                                    <div className="song-list-title">
-                                        <p>{song.title}</p>
-                                    </div>
-                                    <div className="song-list-artist">
-                                        {song.createdBy}
-                                    </div>
+                                <div className="song-list-artist">
+                                    {song.createdBy}
                                 </div>
-                            </section>
-                            <div className="song-list-add-date">
-                                {utilService.timeConverter(song.addedAt)}
                             </div>
-                            <button className="add-song-station song-action " title={titleSvgLiked} onClick={() => toggleLikedSong(song)}>
-                                <svg id="song-liked-svg" className={classSvgLiked} role="img" height="24" width="24" aria-hidden="true" >
-                                    <path d="M15.724 4.22A4.313 4.313 0 0012.192.814a4.269 4.269 0 00-3.622 1.13.837.837 0 01-1.14 0 4.272 4.272 0 00-6.21 5.855l5.916 7.05a1.128 1.128 0 001.727 0l5.916-7.05a4.228 4.228 0 00.945-3.577z"></path>
-                                </svg>
-                            </button>
-                            <div className="song-list-duration">
-                                {utilService.getTimeFromSeconds(song.duration)}
-                            </div>
-                            <button className="remove-song-from-station song-action" onClick={() => onRemoveSongFronStation(idx)}>
-                                X
-                            </button>
+                        </section>
+                        <div className="song-list-add-date">
+                            {utilService.timeConverter(song.addedAt)}
+                        </div>
+                        <button className="add-song-station song-action " title={titleSvgLiked} onClick={() => toggleLikedSong(song)}>
+                            <svg id="song-liked-svg" className={classSvgLiked} role="img" height="24" width="24" aria-hidden="true" >
+                                {/* <path d="M15.724 4.22A4.313 4.313 0 0012.192.814a4.269 4.269 0 00-3.622 1.13.837.837 0 01-1.14 0 4.272 4.272 0 00-6.21 5.855l5.916 7.05a1.128 1.128 0 001.727 0l5.916-7.05a4.228 4.228 0 00.945-3.577z"></path> */}
+                                <path d="M 1.69 2 A 4.582 4.582 0 0 1 8 2.023 A 4.583 4.583 0 0 1 11.88 0.817 h 0.002 a 4.618 4.618 0 0 1 3.782 3.65 v 0.003 a 4.543 4.543 0 0 1 -1.011 3.84 L 9.35 14.629 a 1.765 1.765 0 0 1 -2.093 0.464 a 1.762 1.762 0 0 1 -0.605 -0.463 L 1.348 8.309 A 4.582 4.582 0 0 1 1.689 2 Z m 3.158 0.252 A 3.082 3.082 0 0 0 2.49 7.337 l 0.005 0.005 L 7.8 13.664 a 0.264 0.264 0 0 0 0.311 0.069 a 0.262 0.262 0 0 0 0.09 -0.069 l 5.312 -6.33 a 3.043 3.043 0 0 0 0.68 -2.573 a 3.118 3.118 0 0 0 -2.551 -2.463 a 3.079 3.079 0 0 0 -2.612 0.816 l -0.007 0.007 a 1.501 1.501 0 0 1 -2.045 0 l -0.009 -0.008 a 3.082 3.082 0 0 0 -2.121 -0.861 Z"></path>
+                            </svg>
+                        </button>
+                        <div className="song-list-duration">
+                            {utilService.getTimeFromSeconds(song.duration)}
+                        </div>
+                        <button className="remove-song-from-station song-action" onClick={() => onRemoveSongFronStation(idx)}>
+                            <i className="fa-solid fa-trash song-remove-icon "></i>
+                        </button>
                     </li>
                 })
                 }
